609c46f6d6b908c4a46934493e676da645b75b59
hayasehiise <kuronegahiise@gmail.com>
2024-04-23 15:13:50 +0800

    update little route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return response()->json([
+        'laravel' => 11,
+        'data' => 'testing API'
+    ], 200);
 });
 
 require __DIR__.'/auth.php';
